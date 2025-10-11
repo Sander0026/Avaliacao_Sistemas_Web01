07d65288a27636ac0db5b0d87899ea7b734076a3
Victor Severiano <162388375+VictorSeveriano@users.noreply.github.com>
2025-10-11 19:44:26 -0300

    Atualização de comentarios e mudança no nome de algumas variaveis

--- functions/atualizar_inventario.py
+++ functions/atualizar_inventario.py
@@ -2,7 +2,7 @@ import json
 import uuid
 import random
 from datetime import datetime
-from zoneinfo import ZoneInfo  
+from zoneinfo import ZoneInfo
 
 # lógica de um microserviço que reduz a quantidade de um produto no estoque após uma compra ser finalizada com sucesso.
 def atualizar_inventario(dados_do_status):
@@ -12,18 +12,18 @@ def atualizar_inventario(dados_do_status):
     pedido_id  = dados_do_status.get('pedido_id', 'N/A')
     produto_id = dados_do_status.get('id_produto', 'N/A')
     quantidade = dados_do_status.get('quantidade', 0)
-    timestamp  = datetime.now(ZoneInfo("America/Sao_Paulo")).strftime("%d-%m-%Y %H:%M:%S")
+    horario_atualizacao  = datetime.now(ZoneInfo("America/Sao_Paulo")).strftime("%d-%m-%Y %H:%M:%S")
 
     # Com status sucesso, o microserviço vai atuar iniciando a atualização de inventário
     if status == "SUCESSO":
 
         # Cria um registro de log no formato JSON
         log_inventario = {
-            "timestamp": timestamp,
-            "nivel": "INFO",
-            "servico": "atualizar_inventario",
-            "mensagem": f"Dando baixa de {quantidade} unidade(s) do produto '{produto_id}' no inventário.",
-            "pedido_id": pedido_id
+            "Horario": horario_atualizacao,
+            "Nivel": "Informativo",
+            "Servico": "atualizar_inventario",
+            "Mensagem": f"Dando baixa de {quantidade} unidade(s) do produto '{produto_id}' no inventário.",
+            "Pedido_id": pedido_id
         }
 
         # O log é impresso em JSON para simular o envio a um sistema de atualização de inventário
